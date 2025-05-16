9523e5b33ed4832d70e2ffb15c05a52f5c9376d7
mike5-goku <dylanmacias173@gmail.com>
2025-05-16 16:39:31 -0700

    mike

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,24 @@
-console.log("mario")
+import { useState } from "react"
+import SearchBar from "./components/SearchBar"
+import ImageList from "./components/ImageList"
+import searchImages from "./api"
+
+function App() {
+  //searchImages('cars')
+  const [images, setImages] = useState([])
+
+  const handleSubmit = async(term) => {
+    console.log('Usted esta buscando con: ', term)
+    const result = await searchImages(term)
+    setImages(result)
+  }
+  return (
+    <>
+      <h1>Pictures App</h1>
+      <SearchBar onSubmit={handleSubmit}/>
+      <ImageList images={images}/>
+    </>
+  )
+}
+
+export default App
